44d97ae06b3894aa89f1d5e92f86e3dd16999f7b
gemsoftial <39755458+gemsoftial@users.noreply.github.com>
2019-05-03 16:31:42 +0900

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
-$user = "id9377486_dianhandiyansah";
-$pass = "gemsoftial45";
-$database = "id9377486_count";
+$user = "root";
+$pass = "";
+$database = "auth";
 
 $conn = mysqli_connect($host, $user, $pass)or die(mysqli_error());
 mysqli_select_db($conn, $database)or die(mysqli_error());
